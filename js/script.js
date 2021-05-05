643293d7e1844c52ab064e8e25a1912d1b26a004
darkmaer92 <69340029+darkmaer92@users.noreply.github.com>
2021-05-05 12:19:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-
 /* add class depending on scroll*/
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        document.querySelectorAll('.nav-list__item-link').forEach((link) => {
+          link.classList.toggle(
+            'nav-list__item-link--active', 
+            link.getAttribute('href').replace('#','') === entry.target.id
+          );
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.7,
+  }
+);
+document.querySelectorAll(".section").forEach(
+  (section) => observer.observe(section),);
+
+  const anchors=document.querySelectorAll('a[href*="#"]');for(let anchor of anchors){anchor.addEventListener("click",function(e){e.preventDefault();const blockID=anchor.getAttribute("href").substr(1);document.getElementById(blockID).scrollIntoView({behavior:"smooth",block:"start",});});}
+
 function fc(){
     let nav = document.querySelector('.navbar');
       if (window.pageYOffset > 950) {
@@ -7,113 +27,11 @@ function fc(){
       } else {
         nav.classList.remove('--active');
       } 
-/*1 */ 
-      let extend = document.querySelector('.extend');
-      if (window.pageYOffset > 1000) {
-        extend.classList.add('--active_extend',);
-      } 
-      else {
-        extend.classList.remove('--active_extend');
-      } 
-
-      let extendIN = document.querySelector('.extend');
-      if (window.pageYOffset > 2300) {
-        extendIN.classList.add('--inactive_extend');
-      } else {
-        extendIN.classList.remove('--inactive_extend');
-      } 
-/*2 */ 
-        let migrate = document.querySelector('.migrate');
-        if (window.pageYOffset > 2300) {
-            migrate.classList.add('--active_migrate',);
-        } 
-        else {
-            migrate.classList.remove('--active_migrate');
-        } 
-
-        let migrateIN = document.querySelector('.migrate');
-        if (window.pageYOffset > 3300) {
-            migrateIN.classList.add('--inactiv_migrate');
-        } else {
-            migrateIN.classList.remove('--inactiv_migrate');
-        } 
-/*3 */ 
-        let integrate = document.querySelector('.integrate');
-        if (window.pageYOffset > 3300) {
-            integrate.classList.add('--active_integrate',);
-        } 
-        else {
-            integrate.classList.remove('--active_integrate');
-        } 
-
-        let integrateIN = document.querySelector('.integrate');
-        if (window.pageYOffset > 3900) {
-            integrateIN.classList.add('--inactiv_migrate');
-        } else {
-            integrateIN.classList.remove('--inactiv_migrate');
-        } 
-/*4 */ 
-        let read = document.querySelector('.read');
-        if (window.pageYOffset > 4400) {
-            read.classList.add('--active_read',);
-        } 
-        else {
-            read.classList.remove('--active_read');
-        } 
-
-        let readIN = document.querySelector('.integrate');
-        if (window.pageYOffset > 4400) {
-            readIN.classList.add('--inactiv_integrate');
-        } else {
-            readIN.classList.remove('--inactiv_integrate');
-        } 
-
   }
   
   window.onscroll=fc;
   fc();
 
 
-
-  
 /*end*/
-/* smooth */
-  const anchors=document.querySelectorAll('a[href*="#"]');for(let anchor of anchors){anchor.addEventListener("click",function(e){e.preventDefault();const blockID=anchor.getAttribute("href").substr(1);document.getElementById(blockID).scrollIntoView({behavior:"smooth",block:"start",});});}
-/*smooth  end */
 
-
-/*      let extend = document.querySelector('.extend');
-      if (window.pageYOffset > 1000) {
-        extend.classList.add('--active_extend',);
-      } 
-      else {
-        extend.classList.remove('--active_extend');
-      } 
-
-      let extendIN = document.querySelector('.extend');
-      if (window.pageYOffset > 2300) {
-        extendIN.classList.add('--inactive_extend');
-      } else {
-        extendIN.classList.remove('--inactive_extend');
-      } 
-
-      let migrate = document.querySelector('.migrate');
-      if (window.pageYOffset > 2300) {
-        migrate.classList.add('--active_migrate');
-      } else {
-        migrate.classList.remove('--active_migrate');
-      } 
-
-      let migrateIN = document.querySelector('.migrate');
-      if (window.pageYOffset > 3500) {
-        migrateIN.classList.add('--inactive_migrate');
-      } else {
-        migrateIN.classList.remove('--inactive_migrate');
-      } 
-
-      let integrate = document.querySelector('.integrate');
-      if (window.pageYOffset > 3500) {
-        integrate.classList.add('--active_integrate',);
-      } else {
-        integrate.classList.remove('--active_integrate');
-      } */
