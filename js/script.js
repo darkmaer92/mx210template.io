883adf7c3724aef4f02a4ec5ed15eeb93562037c
darkmaer92 <69340029+darkmaer92@users.noreply.github.com>
2021-06-11 16:17:01 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 /* add class depending on scroll*/
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(
+  (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        document.querySelectorAll('.nav-list__item-link').forEach((link) => {
+        document.querySelectorAll(".nav-list__item-link").forEach((link) => {
           link.classList.toggle(
-            'nav-list__item-link--active', 
-            link.getAttribute('href').replace('#','') === entry.target.id
+            "nav-list__item-link--active",
+            link.getAttribute("href").replace("#", "") === entry.target.id
           );
         });
       }
@@ -15,23 +16,32 @@ const observer = new IntersectionObserver((entries) => {
     threshold: 0.7,
   }
 );
-document.querySelectorAll(".section").forEach(
-  (section) => observer.observe(section),);
+document
+  .querySelectorAll(".section")
+  .forEach((section) => observer.observe(section));
 
-  const anchors=document.querySelectorAll('a[href*="#"]');for(let anchor of anchors){anchor.addEventListener("click",function(e){e.preventDefault();const blockID=anchor.getAttribute("href").substr(1);document.getElementById(blockID).scrollIntoView({behavior:"smooth",block:"start",});});}
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const blockID = anchor.getAttribute("href").substr(1);
+    document
+      .getElementById(blockID)
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+}
 
-function fc(){
-    let nav = document.querySelector('.navbar');
-      if (window.pageYOffset > 950) {
-         nav.classList.add('--active');
-      } else {
-        nav.classList.remove('--active');
-      } 
+function fc() {
+  let nav = document.querySelector(".navbar");
+  if (window.pageYOffset > 950) {
+    nav.classList.add("--active");
+  } else {
+    nav.classList.remove("--active");
   }
-  
-  window.onscroll=fc;
-  fc();
+}
 
+window.onscroll = fc;
+fc();
 
 /*end*/
 
@@ -39,11 +49,11 @@ function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
@@ -51,3 +61,10 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+new Splide(".splide").mount();
+
+var elms = document.getElementsByClassName("splide");
+for (var i = 0, len = elms.length; i < len; i++) {
+  new Splide(elms[i]).mount();
+}
